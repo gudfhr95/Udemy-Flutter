d6da85611d67f28b6965e9458faa670a86b0a287
Hyungrok Ham <ghldnjsdlf@gmail.com>
2021-11-16 23:27:30 +0900

    47. Introducing "if" Statements

--- lib/main.dart
+++ lib/main.dart
@@ -18,32 +18,37 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  final questions = const [
+    {
+      "questionText": "What's your favorite color?",
+      "answers": ["Black", "Red", "Green", "White"],
+    },
+    {
+      "questionText": "What's your favorite animal?",
+      "answers": ["Rabbit", "Snake", "Elephant", "Lion"],
+    },
+    {
+      "questionText": "Who's your favorite instructor",
+      "answers": ["Max", "Max", "Max", "Max"],
+    },
+  ];
   var _questionIndex = 0;
 
   void _answerQuestion() {
+    // var aBool = true;
+    // aBool = false;
+
     setState(() {
       _questionIndex = _questionIndex + 1;
     });
     print(_questionIndex);
+    if (_questionIndex < questions.length) {
+      print("We have more questions!");
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    const questions = [
-      {
-        "questionText": "What's your favorite color?",
-        "answers": ["Black", "Red", "Green", "White"],
-      },
-      {
-        "questionText": "What's your favorite animal?",
-        "answers": ["Rabbit", "Snake", "Elephant", "Lion"],
-      },
-      {
-        "questionText": "Who's your favorite instructor",
-        "answers": ["Max", "Max", "Max", "Max"],
-      },
-    ];
-
     // var dummy = const ["Hello"];
     // dummy.add("Max");
     // print(dummy);
