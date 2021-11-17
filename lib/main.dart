c405048bb1f54a3c8f75e86f0cd02a79fcf16631
Hyungrok Ham <ghldnjsdlf@gmail.com>
2021-11-17 22:34:52 +0900

    85. Fetching User Input

--- lib/main.dart
+++ lib/main.dart
@@ -30,6 +30,10 @@ class MyHomePage extends StatelessWidget {
       date: DateTime.now(),
     ),
   ];
+  // String titleInput;
+  // String amountInput;
+  final titleController = TextEditingController();
+  final amountController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -60,16 +64,24 @@ class MyHomePage extends StatelessWidget {
                       decoration: InputDecoration(
                         labelText: "Title",
                       ),
+                      controller: titleController,
+                      // onChanged: (val) {
+                      //   titleInput = val;
+                      // },
                     ),
                     TextField(
                       decoration: InputDecoration(
                         labelText: "Amount",
                       ),
+                      controller: amountController,
+                      // onChanged: (val) => amountInput = val,
                     ),
                     FlatButton(
                       child: Text("Add Transaction"),
                       textColor: Colors.purple,
-                      onPressed: () {},
+                      onPressed: () {
+                        print(titleController.text);
+                      },
                     )
                   ],
                 ),
