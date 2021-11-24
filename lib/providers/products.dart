8da3b814b93735643049f4b93dad86f34b33c863
Hyungrok Ham <ghldnjsdlf@gmail.com>
2021-11-24 23:58:05 +0900

    251. Updating Data via PATCH Requests

--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -99,16 +99,14 @@ class Products with ChangeNotifier {
       '/products.json',
     );
     try {
-      final response = await http.post(
-        url,
-        body: json.encode({
-          'title': product.title,
-          'description': product.description,
-          'imageUrl': product.imageUrl,
-          'price': product.price,
-          'isFavorite': product.isFavorite,
-        }),
-      );
+      final response = await http.post(url,
+          body: json.encode({
+            'title': product.title,
+            'description': product.description,
+            'imageUrl': product.imageUrl,
+            'price': product.price,
+            'isFavorite': product.isFavorite,
+          }));
       final newProduct = Product(
         title: product.title,
         description: product.description,
@@ -125,9 +123,20 @@ class Products with ChangeNotifier {
     }
   }
 
-  void updateProduct(String id, Product newProduct) {
+  Future<void> updateProduct(String id, Product newProduct) async {
     final prodIndex = _items.indexWhere((prod) => prod.id == id);
     if (prodIndex > 0) {
+      final url = Uri.https(
+        'flutter-shop-efd37-default-rtdb.asia-southeast1.firebasedatabase.app',
+        '/products/${id}.json',
+      );
+      await http.patch(url,
+          body: json.encode({
+            'title': newProduct.title,
+            'description': newProduct.description,
+            'imageUrl': newProduct.imageUrl,
+            'price': newProduct.price,
+          }));
       _items[prodIndex] = newProduct;
       notifyListeners();
     } else {
